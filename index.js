e775d41efea606b2c5d8aa3ce40eb40581cd242e
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-03-25 07:48:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    const found = array.find(array => array.result === `W`);
+    if (found) {
+        return found.year
+    } else {
+        return undefined;
+    }
+}
